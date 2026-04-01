2515315fe98c7f290484bf25983159672b63e7a1
Ayushkesarwani233 <sarvamgoel@pacefin.in>
2026-04-01 15:28:03 +0530

    test commit

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { createBooking } = require('../controllers/bookingController');
+//
 
 // Handles POST requests to /bookings
 router.route('/')
